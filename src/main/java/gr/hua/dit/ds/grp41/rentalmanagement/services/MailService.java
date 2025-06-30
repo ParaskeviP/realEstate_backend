77021573802da769f96a0fc8567919b87ee49de6
Vasilis Kokotakis <vasiliskokotakis2004@gmail.com>
2025-07-01 01:10:39 +0300

    Added MailService

--- src/main/java/gr/hua/dit/ds/grp41/rentalmanagement/services/MailService.java
+++ src/main/java/gr/hua/dit/ds/grp41/rentalmanagement/services/MailService.java
@@ -1,4 +1,26 @@
 package gr.hua.dit.ds.grp41.rentalmanagement.services;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public MailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String to, String subject, String context){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("it2022040@hua.gr");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(context);
+
+        mailSender.send(message);
+    }
 }
